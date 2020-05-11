06dbf5ed0ea1b44526c186f8ec8d0d2136654e63
Nazik92 <60100178+Nazik92@users.noreply.github.com>
2020-05-11 15:54:02 +0300

    Create mario2.c

---
+++ mario2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+   int n;
+   do
+   {
+       n = get_int("Number 1-8: ");
+   }
+   while (n <= 0 || n >= 9);
+   for (int i= 0; i < n; i++)
+   {
+       for (int k=1; k < (n-i); k++)  
+       {
+           printf(" ");
+       } 
+       for (int j=0; j <= i; j++)
+       {
+           printf("#");
+       } 
+       printf("  ");
+       for (int j=0; j <= i; j++)
+       {
+           printf("#");
+       } 
+       printf("\n");
+   } 
+}
