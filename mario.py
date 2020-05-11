65c3f333ab29e57c0dd250db2f70c35910d91c2d
Nazik92 <60100178+Nazik92@users.noreply.github.com>
2020-05-11 16:31:17 +0300

    Create mario.py

---
+++ mario.py
@@ -0,0 +1,22 @@
+from cs50 import get_int
+
+def main():
+    n = get_good_int("Height: ")
+
+    for i in range(n):
+        for j in range(n - i -1):
+            print (" ", end="")
+        for k in range(i + 1):
+            print ("#", end="")
+        print ("")
+
+
+def get_good_int(promt):
+    while True:
+        g = get_int(promt)
+        if g > 0 and g < 9:
+            break
+    return g
+
+if __name__ == "__main__":
+    main()
